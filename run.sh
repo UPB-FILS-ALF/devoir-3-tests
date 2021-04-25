6d3bcac9ba7c6ea3615b63ddc480c4f4f21a4988
Alexandru Radovici <msg4alex@gmail.com>
2021-04-25 15:13:53 +0300

    added checker

--- run.sh
+++ run.sh
@@ -1,60 +1,30 @@
 #!/bin/bash
 
-echo "Run is no working, check the alf language source files"
-exit 1
-
 dir="$(dirname $0)"
 
 function run_test {
     rm -rf output.*
     filename="$dir/tests/$1"
 
-    echo $filename
+    # echo $filename
     
-    outputname="$(dirname $filename)/$(basename $1 .dsn).out"
-    svgoutputname="$(dirname $filename)/$(basename $1 .dsn).svg"
+    outputname="$(dirname $filename)/$(basename $1 .alf).out"
+    astoutputname="$(dirname $filename)/$(basename $1 .alf).ast.json"
     echo Running $filename
-    node $dir/../index.js $filename output.svg > output.out 2> output.err
+    node $dir/../index.js $filename output.ast.json &> $outputname
     ERROR=0
 
-    PIXELS=10
-
-    compare -verbose -metric AE "$svgoutputname" "output.svg" > /dev/null 2> output.report - 
-    tail -n 5 "output.report" > "output.report.tmp"
-    mv "output.report.tmp" "output.report"
-    red_pixels=`cat "output.report" | grep red | head -1 | cut -d ":" -f 2 | tr -d " "`
-    green_pixels=`cat "output.report" | grep green | head -1 | cut -d ":" -f 2 | tr -d " "`
-    blue_pixels=`cat "output.report" | grep blue | head -1 | cut -d ":" -f 2 | tr -d " "`
-    alpha_pixels=`cat "output.report" | grep alpha | head -1 | cut -d ":" -f 2 | tr -d " "`
-    all_pixels=`cat "output.report" | grep all | head -1 | cut -d ":" -f 2 | tr -d " "`
-    # echo "all pixels " "{"$all_pixels"}"
-    if test "$all_pixels" != "" && [[ "$all_pixels" -le "$PIXELS" ]];
+    if node verify.js "$astoutputname" "output.ast.json" &> output.report;
     then
-        cat "$outputname" | sort > output.original.sorted
-        cat output.out | sort > output.sorted
-        if ! diff --ignore-space-change --side-by-side --suppress-common-lines output.sorted output.original.sorted &> error
-        then
-            echo "Your output                                                   | Correct output"
-            cat error
-            ERROR=1
-        else
-            echo Correct
-        fi
+       echo "Correct"
     else
+        cat $outputname
+        cat output.report
+        echo "Your output                                                   | Correct output"
+        diff --ignore-space-change --side-by-side --suppress-common-lines "output.ast.json" "$astoutputname"
         ERROR=1
-        if [ "$all_pixels" != "" ];
-        then
-            echo "Pixels that are different"
-            head -10 output.report
-        else
-            echo "Your SVG file has errors"
-            head -10 output.report
-            echo "Yout output (last 10 lines)"
-            tail -10 output.out
-            echo "Yout errors (last 10 lines)"
-            tail -10 output.err
-        fi
     fi
+    rm $outputname
     rm -rf output.* 
     rm -rf error
     return $ERROR
@@ -63,11 +33,11 @@ function run_test {
 if [ $# -lt 1 ];
 then
     echo "Running all tests"
-    for folder in $(cd $dir && find tests -mindepth 1 -maxdepth 1 -type d)
+    for folder in $(cd $dir && find tests -mindepth 1 -maxdepth 1 -type d | sort)
     do
-        for file in $(cd $dir/$folder && find . -mindepth 1 -maxdepth 2 -type f -name '*.dsn')
+        for file in $(cd $dir/$folder && find . -mindepth 1 -maxdepth 2 -type f -name '*.alf')
         do
-            echo file $(basename $folder)/$(basename $file)
+            # echo file $(basename $folder)/$(basename $file)
             run_test $(basename $folder)/$(basename $file)
         done
     done
---
+++ verify.js
@@ -0,0 +1,69 @@
+"use strict";
+
+let assert = require ('assert');
+let util = require ('util');
+
+let originalFile = process.argv[2];
+let verifyFile = process.argv[3];
+
+let originalTree;
+let verifyTree;
+
+try
+{
+	originalTree = require ('./'+originalFile);
+}
+catch (e)
+{
+	console.error ('Cannot load original AST from '+originalFile);
+	process.exit (-1);
+}
+
+try
+{
+	verifyTree = require ('./'+verifyFile);
+}
+catch (e)
+{
+	console.error ('Cannot load AST from '+verifyFile);
+	process.exit (-1);
+}
+
+if (!originalTree.errors)
+{
+	assert.deepStrictEqual (originalTree, verifyTree);
+}
+else
+{
+	if (Array.isArray (verifyTree.errors))
+	{
+		if (originalTree.errors.length === verifyTree.errors.length) {
+			for (let error of originalTree.errors) {
+				let gotit = false;
+				for (let err of verifyTree.errors) {
+					if (error.id === err.id && error.line === err.line && error.position === err.position) {
+						gotit = true;
+					}
+				}
+				if (gotit === false) {
+					console.log (`You did not report ${JSON.stringify (error)}`);
+					process.exit (1);
+				}
+			}
+		}
+		else
+		{
+			console.log (`There should be ${originalTree.errors.length} errors, you reported  ${verifyTree.errors.length}`);
+			process.exit (1);
+		}
+		
+	}
+	else
+	{
+		console.log (`Errors should be an array, you have ${typeof (verifyTree.errors)}`);
+		process.exit (1);
+	}
+}
+
+
+
